7c64e18c7a39135e888a710736de770933cabbda
Rodrigo Zandonadi <rodrigo.zandonadi@acticonsultoria.com.br>
2025-09-25 11:55:23 -0300

    Correction

--- api/src/index.js
+++ api/src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const RTMP_URL = process.env.RTMP_URL;
 const BASE_VIDEO = process.env.BASE_VIDEO || '/srv/lofi/video/christian-lofi.mp4';
-const AUDIO_FILE = process.env.AUDIO_FILE || '/srv/lofi/audio/lofi-worship-playlist.mp3';
+const AUDIO_FILE = process.env.AUDIO_FILE || '/srv/lofi/audio/lofi-worship-playlist.m4a';
 const LIVE_TITLE = process.env.LIVE_TITLE || 'Aslan Lofi Worship Chill Music  #lofi #lofichillbeats #lofiworship';
 const CHANNEL_NAME = process.env.CHANNEL_NAME || 'Aslan Lofi Worship';
 const OVERLAY_TRACK_TEMPLATE = process.env.OVERLAY_TRACK_TEMPLATE || '{title} — {artist}';
